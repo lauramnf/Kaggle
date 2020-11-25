1a540ac83001cff8dd3931b71e6aa4e4f42f62fc
Laura Maciel <laura.neves2008@hotmail.com>
2020-11-25 12:14:48 -0200

    Fixing bugs

--- Titanic.py
+++ Titanic.py
@@ -3,33 +3,34 @@ import pandas as pd # data processing
 import os #interact with operational system
 from sklearn.linear_model import LogisticRegression #machine learning model
 
-# Showing files
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-
 # Oppening training files
-arquivo_treino = pd.read_csv("/kaggle/input/titanic/train.csv")
-
+arquivo_treino = pd.read_csv("train.csv")
 # Openning testing file
-arquivo_teste = pd.read_csv("/kaggle/input/titanic/test.csv")
+arquivo_teste = pd.read_csv("test.csv")
 
-# Verify missing data in training and testing files (null elements)
+passenger_id = arquivo_teste['PassengerId'] #Saving Passenger ID
+# Delete useless data
+arquivo_treino = arquivo_treino.drop(columns=['Name', 'Cabin', 'PassengerId', 'Ticket'])
+arquivo_teste = arquivo_teste.drop(columns=['Name', 'Cabin', 'PassengerId', 'Ticket'])
+
+# Verify missing data in files (null elements)
+# Training data
 arquivo_treino[arquivo_treino.isnull().any(axis=1)]
+# Testing data
 arquivo_teste[arquivo_teste.isnull().any(axis=1)]
 
-# Fill the missing in training and testing data
-arquivo_treino = arquivo_treino.fillna({"Cabin": "nao identificado"})
+# Fill the missing data
+# Training data
 ageNa = arquivo_treino['Age'].mean(skipna = True)
+arquivo_treino = arquivo_treino.fillna({'Fare': 0})
 arquivo_treino = arquivo_treino.fillna({'Age': ageNa})
-
-arquivo_teste = arquivo_teste.fillna({"Cabin": "nao identificado"})
+# Testing data
+arquivo_teste = arquivo_teste.fillna({'Fare': 0})
 arquivo_teste = arquivo_teste.fillna({'Age': ageNa})
 
 # Use One Hot Enconding module to change categorical data into numerical data in both files
-arquivo_atualizado = pd.get_dummies(arquivo_treino, columns=["Name", "Sex", "Ticket", "Cabin", "Embarked"], prefix=["name", "sex", "ticket", "cabin", "embarked"], dtype=np.int64)
-teste_atualizado = pd.get_dummies(arquivo_teste, columns=["Name", "Sex", "Ticket", "Cabin", "Embarked"], prefix=["name", "sex", "ticket", "cabin", "embarked"], dtype=np.int64)
-
+teste_atualizado = pd.get_dummies(arquivo_teste, columns=["Sex","Embarked"], prefix=["sex", "embarked"], dtype=np.int64)
+arquivo_atualizado = pd.get_dummies(arquivo_treino, columns=["Sex", "Embarked"], prefix=["sex", "embarked"], dtype=np.float64)
 
 # Get the testing data and the corresponding result separately
 y_treino = (arquivo_atualizado['Survived']).values
@@ -42,6 +43,6 @@ x_teste = teste_atualizado.values
 molde = LogisticRegression()
 molde.fit(x_treino,y_treino) #Fiting training data into the module
 y_teste = molde.predict(x_teste) # Predict testing data results
-saida = pd.DataFrame({'PassengerId': teste_atualizado.PassengerId, 'Survived': y_teste})
+saida = pd.DataFrame({'PassengerId': passenger_id, 'Survived': y_teste})
 saida.to_csv('my_submission.csv', index=False)
 print("Your submission was successfully saved!")
